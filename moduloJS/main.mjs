0403c90e584724f7317c233ed6e86543f04f1a03
LL <lucasluann10@gmail.com>
2022-03-10 13:22:19 -0400

    Revisando Modulos em JS

---
+++ moduloJS/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade,mostraCidade,mostraHobby} from './funcoes.mjs';
+
+console.log(mostraIdade('Lucas', 21))
+console.log(mostraCidade('Lucas', 'Cuiabá'))
+console.log(mostraHobby('Lucas', 'Jogar'))
