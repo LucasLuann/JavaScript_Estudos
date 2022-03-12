7f21d945f0447b1035965529e66e1aff6f97a2e7
LL <lucasluann10@gmail.com>
2022-03-11 23:10:35 -0400

    Praticando  POO

---
+++ POO/scripts.js
@@ -0,0 +1,25 @@
+class contaBancaria {
+    constructor(agencia, numero, tipo, saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = saldo;    
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return 'Operação negada';
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+}
